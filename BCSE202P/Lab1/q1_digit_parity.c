740fffc9c4047d1dcff6236331033115b85c1aa8
Andhavarapu Balu <balu.somenumbers@gmail.com>
2023-08-02 13:30:00 +0530

    q1 done

--- BCSE202P/Lab1/q1_digit_parity.c
+++ BCSE202P/Lab1/q1_digit_parity.c
@@ -1,9 +1,20 @@
 #include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 int isOdd(int number){
     return number%2;
 }
 
 int getDigit(int number, int index){
-    int number_left_of_index = number / pow(10, 0);
+    number = abs(number);
+    int number_with_left_part_of_digit_removed = number % (int)(pow(10, index));
+    int digit = number_with_left_part_of_digit_removed/(int)pow(10, index-1);
+    return digit;
+}
+
+int main(){
+    int n, p;
+    scanf("%d\n%d", &n, &p);
+    printf(isOdd(getDigit(n, p)) ? "Odd\n" : "Even\n");
 }
