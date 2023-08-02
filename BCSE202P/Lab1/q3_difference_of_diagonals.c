33dad2fbf8deedcb008dca95e288a9b209763b71
Andhavarapu Balu <balu.somenumbers@gmail.com>
2023-08-02 14:42:35 +0530

    q3 code to be able to access sums of diagonals too

--- BCSE202P/Lab1/q3_difference_of_diagonals.c
+++ BCSE202P/Lab1/q3_difference_of_diagonals.c
@@ -3,14 +3,17 @@
 
 // Alternative: Calculate the sums while reading the array itself without ever having to store the elements
 
-int find_difference_of_diagonals(int n, int matrix[n][n]){
-    int sum_of_left_diagonal = 0;
-    int sum_of_right_diagonal = 0;
+void find_sum_of_diagonal(int *sum_of_dexter_diagonal, int *sum_of_sinister_diagonal, int n, int matrix[n][n]){
+    *sum_of_dexter_diagonal = 0;
+    *sum_of_sinister_diagonal = 0;
     for(int i=0; i<n; ++i){
-        sum_of_left_diagonal += matrix[i][i];
-        sum_of_right_diagonal += matrix[i][n-i-1];
+        *sum_of_dexter_diagonal += matrix[i][i];
+        *sum_of_sinister_diagonal += matrix[i][n-i-1];
     }
-    return abs(sum_of_left_diagonal - sum_of_right_diagonal);
+}
+
+int find_difference_of_diagonals(int sum_of_dexter_diagonal, int sum_of_sinister_diagonal, int n, int matrix[n][n]){
+    return abs(sum_of_dexter_diagonal - sum_of_sinister_diagonal);
 }
 
 int main(){
@@ -22,5 +25,11 @@ int main(){
             scanf("%d", matrix[i]+j);
         }
     }
-    printf("%d\n", find_difference_of_diagonals(n, matrix));
+    int sum_of_dexter_diagonal, sum_of_sinister_diagonal;
+    find_sum_of_diagonal(&sum_of_dexter_diagonal, &sum_of_sinister_diagonal, n, matrix);
+    printf("%d\n%d\n%d\n", 
+        sum_of_dexter_diagonal, 
+        sum_of_sinister_diagonal, 
+        find_difference_of_diagonals(sum_of_dexter_diagonal, sum_of_sinister_diagonal, n, matrix)
+    );
 }
