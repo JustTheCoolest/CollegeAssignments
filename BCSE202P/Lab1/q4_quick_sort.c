9d0bcf4ff8f0db7a12a7530b8be4a6ef07872561
Andhavarapu Balu <balu.somenumbers@gmail.com>
2023-08-03 18:18:40 +0530

    Reorganised

--- BCSE202P/Lab1/q4_quick_sort.c
+++ BCSE202P/Lab1/q4_quick_sort.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "../../Really Useful/QuickSort.c"
 
 void getInputs(int n, int numbers[]){
     for(int i=0; i<n; i++){
@@ -6,58 +7,11 @@ void getInputs(int n, int numbers[]){
     }
 }
 
-void swap(int *a, int *b){
-    int temp = *a;
-    *a = *b;
-    *b = temp; 
-}
-
-int incrementedTraverser(int numbers[], int traverser, int pivot, int index_limit){
-    int direction = index_limit - traverser > 0 ? 1 : -1;
-    while(numbers[traverser]*direction <= pivot*direction && traverser*direction < index_limit*direction){
-        traverser += direction;
-    }
-    return traverser;
-}
-
-int isInRange(int current_position, int start_position, int end_position){
-    return current_position >= start_position && current_position <= end_position;
-}
-
 void printArray(int n, int numbers[], int k1, int k2){
     for(int i=k1; i<=k2; ++i){
         printf("%d ", numbers[i]);
     }
     printf("\n");
-}
-
-// Inference: Cannot use a swap flag to break loop because choice of pivot influences number of swaps too
-void quickSort(int numbers[], int start_index, int end_index){
-    //printArray(10, numbers, 0, 9);
-    int pivot = numbers[start_index];
-    int left_traverser = start_index;
-    int right_traverser = end_index;
-    while(1){
-        left_traverser = incrementedTraverser(numbers, left_traverser, pivot, end_index);
-        right_traverser = incrementedTraverser(numbers, right_traverser, pivot, start_index);
-        if(left_traverser >= right_traverser){
-            break;
-        }
-        swap(&numbers[left_traverser], &numbers[right_traverser]);
-    }
-    if(start_index != right_traverser){
-        swap(&numbers[start_index], &numbers[right_traverser]);
-    }
-    int left_segment_end_index = right_traverser-1;
-    if(isInRange(left_segment_end_index, start_index, end_index)){
-        //printf("%d %d\n", start_index, left_segment_end_index);
-        quickSort(numbers, start_index, left_segment_end_index);
-    }
-    int right_segment_start_index = right_traverser+1;
-    if(isInRange(right_segment_start_index, start_index, end_index)){
-        //printf("%d %d\n", right_segment_start_index, end_index);
-        quickSort(numbers, right_segment_start_index, end_index);
-    }
 }
 
 int test_main(){
@@ -70,7 +24,7 @@ int test_main(){
     printArray(n, numbers, k1, k2);
 }
 
-int main(){
+int actual_main(){
     int n;
     scanf("%d", &n);
     int numbers[n];
@@ -80,4 +34,8 @@ int main(){
     quickSort(numbers, 0, n-1);
     printArray(n, numbers, 0, n-1);
     printArray(n, numbers, k1, k2);
+}
+
+int main(){
+    test_main();
 }
---
+++ Really Useful/QuickSort.c
@@ -0,0 +1,46 @@
+void swap(int *a, int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp; 
+}
+
+int incrementedTraverser(int numbers[], int traverser, int pivot, int index_limit){
+    int direction = index_limit - traverser > 0 ? 1 : -1;
+    while(numbers[traverser]*direction <= pivot*direction && traverser*direction < index_limit*direction){
+        traverser += direction;
+    }
+    return traverser;
+}
+
+int isInRange(int current_position, int start_position, int end_position){
+    return current_position >= start_position && current_position <= end_position;
+}
+
+// Inference: Cannot use a swap flag to break loop because choice of pivot influences number of swaps too
+void quickSort(int numbers[], int start_index, int end_index){
+    //printArray(10, numbers, 0, 9);
+    int pivot = numbers[start_index];
+    int left_traverser = start_index;
+    int right_traverser = end_index;
+    while(1){
+        left_traverser = incrementedTraverser(numbers, left_traverser, pivot, end_index);
+        right_traverser = incrementedTraverser(numbers, right_traverser, pivot, start_index);
+        if(left_traverser >= right_traverser){
+            break;
+        }
+        swap(&numbers[left_traverser], &numbers[right_traverser]);
+    }
+    if(start_index != right_traverser){
+        swap(&numbers[start_index], &numbers[right_traverser]);
+    }
+    int left_segment_end_index = right_traverser-1;
+    if(isInRange(left_segment_end_index, start_index, end_index)){
+        //printf("%d %d\n", start_index, left_segment_end_index);
+        quickSort(numbers, start_index, left_segment_end_index);
+    }
+    int right_segment_start_index = right_traverser+1;
+    if(isInRange(right_segment_start_index, start_index, end_index)){
+        //printf("%d %d\n", right_segment_start_index, end_index);
+        quickSort(numbers, right_segment_start_index, end_index);
+    }
+}
