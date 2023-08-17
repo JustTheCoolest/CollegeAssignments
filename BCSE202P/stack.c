2f38afab96b5e7c984f0ec6b15bb4ec752ac8a1a
Andhavarapu Balu <74148176+JustTheCoolest@users.noreply.github.com>
2023-08-17 20:04:09 +0530

    Add files via upload

---
+++ BCSE202P/stack.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+
+int outOfLimits(int const n, int const top){
+    if(top==n){
+        printf("Stack overflow\n");
+        return -1;
+    }
+    if(top==0){
+        printf("Stack underflow\n");
+        return 1;
+    }
+    return 0;
+}
+
+void push(int const n, int stack[], int * const top, int const element){
+    if(outOfLimits(n, *top)) return;
+    stack[(*top)++] = element;
+}
+
+int pop(int const n, int stack[], int * const top){
+    if(outOfLimits(n, *top)) return;
+    return stack[--(*top)];
+}
+
+int seek(int const n, int const stack[], int const * const top){
+    if(outOfLimits(n, *top)) return;
+    return stack[*top];
+}
+
+void display(int const n, int const stack[], int const *top){
+    if(outOfLimits(n, *top) == -1){
+        printf("There are no elements in the stack\n");
+        return;
+    }
+    for(int i=0; i<top; ++i){
+        printf("%d ", stack[i]);
+    }
+    printf("<- Top\n");
+}
