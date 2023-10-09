17b315b04816b737dcdf9fb3c39e17e2fd0810ae
Andhavarapu Balu <74148176+JustTheCoolest@users.noreply.github.com>
2023-10-09 06:50:27 +0530

    sync

--- MPMC/Project/main.ino
+++ MPMC/Project/main.ino
@@ -1,8 +1,13 @@
 #include <Arduino.h>
 
+constexpr unsigned int baud_rate = 9600;
+constexpr unsigned int rounds_calibration = 500;
+constexpr unsigned int delay_calibration = 0;
+constexpr unsigned float ratio_calibration = 0.3;
+
 constexpr byte flame_sensor_pin = A0;
 constexpr byte led_pin = LED_BUILTIN;
-constexpr int baud_rate = 9600;
+int threshold;
 
 void setUpFlameSensor(byte flame_sensor_pin){
   pinMode(flame_sensor_pin, INPUT);
@@ -17,10 +22,22 @@ void setUpLed(byte led_pin){
 }
 
 void calibrate(byte flame_sensor_pin){
-    
+    int min = __INT_MAX__, max = -1;
+    for(int i=0; i<rounds_calibration; i++){
+        int flame_sensor_value = readFlameSensor(flame_sensor_pin);
+        if(flame_sensor_value > max){
+            max = flame_sensor_value;
+        }
+        if(flame_sensor_value < min){
+            min = flame_sensor_value;
+        }
+        delay(delay_calibration);
+    }
+    int threshold = ratio_calibration * (max - min) + min;
+    return threshold;
 }
 
-void calibration_driver(byte flame_sensor_pin){
+ int ui_calibration(byte flame_sensor_pin){
     Serial.println("Calibration awaited.");
     Serial.println("Instructions: At the intended distance from the flame, move the sensor in oscillating motion, passing through the flame and region outside the flame too\n");
     char input;
@@ -33,17 +50,19 @@ void calibration_driver(byte flame_sensor_pin){
         }
     } while(input != 'y');
     Serial.println("Calibration starts");
-    calibrate(flame_sensor_pin);
+    int threshold = calibrate(flame_sensor_pin);
+    Serial.println("Calibration ends with threshold: " + String(threshold));
 } 
 
 void setup(){
   setUpFlameSensor(flame_sensor_pin);
   setUpLed(led_pin);
   Serial.begin(baud_rate);
-  calibrate(flame_sensor_pin);
+  threshold = calibrate(flame_sensor_pin);
 }
 
 void loop(){
+  static bool flag;
   int flame_sensor_value = analogRead(flame_sensor_pin);
   Serial.println(flame_sensor_value);
   if(flame_sensor_value > 500){
