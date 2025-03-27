b8b5263340f75115dda5aec40f05c2884896642b
JustTheCoolest <balu.somenumbers@gmail.com>
2025-03-27 22:13:56 +0530

    all cases dag

--- BCSE307P_CompilerDesign/dag.cpp
+++ BCSE307P_CompilerDesign/dag.cpp
@@ -159,7 +159,7 @@ void display(Node* root){
 }
 
 int main(){
-    "a= (b+-c) * (b+-c) +d";
+    cout << "a= (b+-c) * (b+-c) +d" << endl;
     Node* n1 = new Node('b');
     Node* n2 = new Node('c');
     Node* n3 = new Node('-', n2);
@@ -173,20 +173,48 @@ int main(){
     Node* n11 = new Node('+', n9, n10);
     Node* n12 = new Node('a');
     Node* n13 = new Node('=', n12, n11);
-
-    // cout << (hash<Node>{}(*n1) == hash<Node>{}(*n5)) << endl;
-    // cout << (hash<Node>{}(*n1) == hash<Node>{}(*n10)) << endl;
-    // cout << (*n1 == *n5) << endl;
-    // cout << (*n1 == *n10) << endl;
-
-    // auto it = PostOrderIterator(n13);
-    // while(it.is_not_end()){
-    //     cout << (*it)->val << " ";
-    //     ++it;
-    // }
-    // cout << endl;
-
     n13 = convert_to_dag(n13);
     display(n13);
+    cout << endl;
     
+    cout << "b = a+d-e *a+b-(c/f)" << endl;
+    Node* n21 = new Node('a');
+    Node* n22 = new Node('d');
+    Node* n23 = new Node('+', n21, n22);
+    Node* n24 = new Node('e');
+    Node* n25 = new Node('-', n23, n24);
+    Node* n26 = new Node('a');
+    Node* n27 = new Node('b');
+    Node* n28 = new Node('+', n26, n27);
+    Node* n29 = new Node('c');
+    Node* n30 = new Node('f');
+    Node* n31 = new Node('/', n29, n30);
+    Node* n32 = new Node('-', n28, n31);
+    Node* n33 = new Node('*', n25, n32);
+    Node* n34 = new Node('b');
+    Node* n35 = new Node('=', n34, n33);
+    n35 = convert_to_dag(n35);
+    display(n35);
+    cout << endl;
+
+    cout << "(n=(m+g)×(h−(i/j))+(k×m)*n" << endl;
+    Node* n41 = new Node('m');
+    Node* n42 = new Node('g');
+    Node* n43 = new Node('+', n41, n42);
+    Node* n44 = new Node('h');
+    Node* n45 = new Node('i');
+    Node* n46 = new Node('j');
+    Node* n47 = new Node('/', n45, n46);
+    Node* n48 = new Node('-', n44, n47);
+    Node* n49 = new Node('*', n43, n48);
+    Node* n50 = new Node('k');
+    Node* n51 = new Node('m');
+    Node* n52 = new Node('*', n50, n51);
+    Node* n53 = new Node('n');
+    Node* n54 = new Node('+', n49, n52);
+    Node* n55 = new Node('n');
+    Node* n56 = new Node('=', n53, n54);
+    n56 = convert_to_dag(n56);
+    display(n56);
+    cout << endl;
 }
