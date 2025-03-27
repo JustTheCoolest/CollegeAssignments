173b5f4e8ed47f37c451557eb268dd69a33bfa0a
JustTheCoolest <balu.somenumbers@gmail.com>
2025-03-27 14:57:23 +0530

    compiling

--- BCSE307P_CompilerDesign/dag.cpp
+++ BCSE307P_CompilerDesign/dag.cpp
@@ -2,53 +2,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// struct node{
-//     node *left, *right;
-// };
-
-// node* convert_to_dag(node* root){
-//     unordered_set<node> nodes = unordered_set<node>();
-//     // vector<node*> path = vector<node*>();
-//     // path.push_back(root);
-//     // while(root->left && root->right){
-
-//     // }
-//     auto queue = vector<pair<node*, node*>>();
-//     queue.push_back({nullptr, root});
-//     auto it = queue.begin();
-//     while(it != queue.end()){
-//         queue.push_back({it->first, it->second->left});
-//         queue.push_back({it->first, it->second->right});
-//         ++it;
-//     }
-
-//     auto rit = queue.rbegin();
-//     while(rit != queue.rend()){
-//         // if(*(*iterator->second) in nodes){
-//         const auto match = nodes.find(it->second);
-//         if(match == queue.rend()){
-//             nodes.insert(it->second);
-//             continue;
-//         }
-//         delete it->second;
-//         it->first
-//     }
-// }
-
 struct Node{
+    char val; // Task: Reimplement using string
     Node* left;
     Node* right;
+    Node(char v, Node* l = nullptr, Node* r = nullptr): val(v), left(l), right(r){}
+
+    bool operator==(const Node& other) const{
+        return val == other.val && left == other.left && right == other.right;
+    }
 };
 
-// Custom post-order iterator
+namespace std{
+    template<>
+    struct hash<Node>{
+        size_t operator()(const Node n) const{
+            return hash<char>{}(n.val) ^ hash<Node*>{}(n.left) ^ hash<Node*>{}(n.right);
+        }
+    };
+};
+
 class PostOrderIterator {
-    std::stack<Node*&> stack;
+    std::stack<std::reference_wrapper<Node*>> stack;
     Node* last_processed = nullptr;
 
 public:
     explicit PostOrderIterator(Node* root) {
         if (root) {
-            stack.push(root);
+            stack.push(std::ref(root));
         }
     }
 
@@ -57,7 +38,7 @@ public:
     }
 
     Node* operator*() const {
-        return stack.top();
+        return stack.top().get();
     }
     
     PostOrderIterator& operator++() {
@@ -65,19 +46,19 @@ public:
         if(stack.empty()){
             return *this;
         }
-        while(stack.top()->left || stack.top()->right){
-            if(stack.top()->right){
-                stack.push(stack.top()->right);
+        while (stack.top().get()->left || stack.top().get()->right) {
+            if (stack.top().get()->right) {
+                stack.push(std::ref(stack.top().get()->right));
             }
-            if(stack.top()->left){
-                stack.push(stack.top()->left);
+            if (stack.top().get()->left) {
+                stack.push(std::ref(stack.top().get()->left));
             }
         }
         return *this;
     }
 
     Node*& get_ref() {
-        return stack.top();
+        return stack.top().get();
     }
 };
 
@@ -93,32 +74,72 @@ Node* convert_to_dag(Node* root) {
             ++it;
             continue;
         }
-        Node*& ref_node = it.get_ref();
+        Node* ref_node = it.get_ref();
         delete *it;
         ref_node = *match;
+        ++it;
     }
 
     return root;
 }
 
+class DAG_LevelOrderIterator{
+    queue<Node*> q;
+
+public:
+    DAG_LevelOrderIterator(Node* root){
+        q = queue<Node*>{};
+        q.push(root);
+    }
+
+    DAG_LevelOrderIterator& operator++(){
+        auto popped = q.front();
+        q.pop();
+        if(popped->left != nullptr){
+            q.push(popped->left);
+        }
+        if(popped->right != nullptr){
+            q.push(popped->right);
+        }
+        return *this;
+    }
+
+    Node* operator*(){
+        return q.front();
+    }
+
+    bool is_not_end(){
+        return q.empty();
+    }
+};
+
+void display(Node* root){
+    auto it = DAG_LevelOrderIterator(root);
+    while(it.is_not_end()){
+        std::cout << (*it)->val << " " << *it << " -> " 
+            << (*it)->left << " " << &(*it)->left << " , " 
+            << (*it)->right << " " << &(*it)->right << "\n";
+        ++it;
+    }
+}
+
 int main(){
     "a= (b+-c) * (b+-c) +d";
-    Node* n1 = new Node("b");
-    Node* n2 = new Node("c");
-    Node* n3 = new Node("-", n2);
-    Node* n4 = new Node("+", n1, n3);
-    Node* n5 = new Node("b");
-    Node* n6 = new Node("c");
-    Node* n7 = new Node("-", n6);
-    Node* n8 = new Node("+", n5, n7);
-    Node* n9 = new Node("*", n4, n8);
-    Node* n10 = new Node("d");
-    Node* n11 = new Node("+", n9, n10);
-    Node* n12 = new Node("a");
-    Node* n13 = new Node("=", n12, n11);
+    Node* n1 = new Node('b');
+    Node* n2 = new Node('c');
+    Node* n3 = new Node('-', n2);
+    Node* n4 = new Node('+', n1, n3);
+    Node* n5 = new Node('b');
+    Node* n6 = new Node('c');
+    Node* n7 = new Node('-', n6);
+    Node* n8 = new Node('+', n5, n7);
+    Node* n9 = new Node('*', n4, n8);
+    Node* n10 = new Node('d');
+    Node* n11 = new Node('+', n9, n10);
+    Node* n12 = new Node('a');
+    Node* n13 = new Node('=', n12, n11);
 
-    Node* n13 = convert_to_dag(n13);
+    n13 = convert_to_dag(n13);
     display(n13);
-
     
 }
