f0e73e50276ec62cd173f45a981e0a9972fd0192
JustTheCoolest <balu.somenumbers@gmail.com>
2025-03-27 20:40:32 +0530

    not working point

--- BCSE307P_CompilerDesign/dag_reprod.cpp
+++ BCSE307P_CompilerDesign/dag_reprod.cpp
@@ -19,55 +19,27 @@ public:
         }
     }
 
-    bool is_not_end() const {
-        return !stack.empty();
-    }
-
-    Node* operator*() const {
+    Node* func() {
         auto gotten_node = stack.top().get();
         cout << "gotten_node: " << gotten_node << endl;
         auto gotten_node2 = stack.top().get();
         cout << "gotten_node2: " << gotten_node2 << endl;
         return gotten_node;
     }
-    
-    PostOrderIterator& operator++() {
-        stack.pop();
-        if(stack.empty()){
-            return *this;
-        }
-        while (stack.top().get()->left || stack.top().get()->right) {
-            if (stack.top().get()->right) {
-                stack.push(std::ref(stack.top().get()->right));
-            }
-            if (stack.top().get()->left) {
-                stack.push(std::ref(stack.top().get()->left));
-            }
-        }
-        return *this;
-    }
 };
 
 int main(){
-    "a= (b+-c) * (b+-c) +d";
-    Node* n1 = new Node('b');
-    Node* n2 = new Node('c');
-    Node* n3 = new Node('-', n2);
-    Node* n4 = new Node('+', n1, n3);
-    Node* n5 = new Node('b');
-    Node* n6 = new Node('c');
-    Node* n7 = new Node('-', n6);
-    Node* n8 = new Node('+', n5, n7);
-    Node* n9 = new Node('*', n4, n8);
+    Node* n9 = new Node('*');
     Node* n10 = new Node('d');
     Node* n11 = new Node('+', n9, n10);
     Node* n12 = new Node('a');
     Node* n13 = new Node('=', n12, n11);
 
+    std::stack<std::reference_wrapper<Node*>> stack;
+    stack.push(std::ref(n13));
+    stack.top().get();
+
     auto it = PostOrderIterator(n13);
-    while(it.is_not_end()){
-        cout << (*it)->val << " ";
-        ++it;
-    }
+    cout << (it.func())->val << " ";
     cout << endl;
 }
